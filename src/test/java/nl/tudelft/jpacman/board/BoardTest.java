6ef5ddf2f04825ab290038e0c8337b19c845d146
Danai Suwantanee <107535705+Danai6588102@users.noreply.github.com>
2025-11-03 23:04:14 +0700

    Create BoardTest.java

---
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -0,0 +1,24 @@
+package nl.tudelft.jpacman.board;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+
+public class BoardTest {
+    @Test
+    void testValidBoard() {
+        Square[][] grid = new Square[1][1];
+        grid[0][0] = new BasicSquare();
+        Board board = new Board(grid);
+
+        assertThat(board.invariant()).isTrue();
+    }
+    @Test
+    void testBoardNullSquare() {
+        Square[][] grid = new Square[1][1];
+        grid[0][0] = null;
+        Board board = new Board(grid);
+
+        assertThat(board.invariant()).isFalse();
+        assertThrows(NullPointerException.class, () -> board.squareAt(0, 0).getOccupants());
+    }
+}
