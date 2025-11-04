b7ae67a0818cd26d23568c27e203756a06bbb908
Danai Suwantanee <107535705+Danai6588102@users.noreply.github.com>
2025-11-04 15:48:27 +0700

    Update BoardTest.java

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -1,6 +1,6 @@
 package nl.tudelft.jpacman.board;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+//import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 /**
